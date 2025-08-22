7c4413f8de6dde03c3c6b645fd3449332b483c09
DenDev1 <dendopulgo123@gmail.com>
2025-08-22 23:48:57 +0800

    Done testing

---
+++ BarakoCups/MyMvc.Test/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using BarakoCups.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace BarakoCups.MyMvc.Test
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsViewResult()
+        {
+            // Arrange  
+            var logger = new LoggerFactory().CreateLogger<HomeController>();
+            var controller = new HomeController(logger);
+
+            // Act  
+            var result = controller.Index();
+
+            Xunit.Assert.IsType<ViewResult>(result); // ✅ Explicitly call xUnit Assert
+        }
+    }
+}
